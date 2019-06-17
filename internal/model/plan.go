fd73fba77eaf4aeffc0d39ed85de250b361680e6
y.t <y.softvalley@gmail.com>
2019-06-17 18:27:58 +0900

    replace plan's func

--- internal/model/plan.go
+++ internal/model/plan.go
@@ -47,34 +47,6 @@ func NewPlan(db *gorm.DB, startStr string, endStr string, title string, descript
 	return p, err
 }
 
-func parseTime(timeString string) (*time.Time, error) {
-	ok, err := regexp.MatchString(`^([0-1][0-9]|2[0-3]):([0-5][0-9])$`, timeString)
-	if err != nil {
-		return nil, err
-	}
-	if !ok {
-		return nil, errors.New(fmt.Sprintf("[%s] is not matched to time format", timeString))
-	}
-	hourAndMinStr := strings.Split(timeString, ":")
-	hour, err := strconv.Atoi(hourAndMinStr[0])
-	if err != nil {
-		return nil, err
-	}
-	min, err := strconv.Atoi(hourAndMinStr[1])
-	if err != nil {
-		return nil, err
-	}
-	n := time.Now()
-	t := time.Date(n.Year(), n.Month(), n.Day(), hour, min, 0, 0, time.Local)
-	return &t, err
-}
-
-func (p Plan) String() string {
-	const layout = "2006-01-02 15:04"
-	const format = "ID : %v\nStart : %v\nEnd : %v\nTitle : %v\nDescription : %v"
-	return fmt.Sprintf(format, p.ID, p.Start.Format(layout), p.End.Format(layout), p.Title, p.Description)
-}
-
 func GetPlan(db *gorm.DB, idStr string) (*Plan, error) {
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
@@ -128,4 +100,32 @@ func (p *Plan) Update(db *gorm.DB, startStr string, endStr string, title string,
 		Description: description,
 	})
 	return p, err
+}
+
+func (p Plan) String() string {
+	const layout = "2006-01-02 15:04"
+	const format = "ID : %v\nStart : %v\nEnd : %v\nTitle : %v\nDescription : %v"
+	return fmt.Sprintf(format, p.ID, p.Start.Format(layout), p.End.Format(layout), p.Title, p.Description)
+}
+
+func parseTime(timeString string) (*time.Time, error) {
+	ok, err := regexp.MatchString(`^([0-1][0-9]|2[0-3]):([0-5][0-9])$`, timeString)
+	if err != nil {
+		return nil, err
+	}
+	if !ok {
+		return nil, errors.New(fmt.Sprintf("[%s] is not matched to time format", timeString))
+	}
+	hourAndMinStr := strings.Split(timeString, ":")
+	hour, err := strconv.Atoi(hourAndMinStr[0])
+	if err != nil {
+		return nil, err
+	}
+	min, err := strconv.Atoi(hourAndMinStr[1])
+	if err != nil {
+		return nil, err
+	}
+	n := time.Now()
+	t := time.Date(n.Year(), n.Month(), n.Day(), hour, min, 0, 0, time.Local)
+	return &t, err
 }
