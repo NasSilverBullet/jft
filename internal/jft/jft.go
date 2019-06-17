2c8530e92f1f3d775f862d57d612e8cbc40d9574
y.t <y.softvalley@gmail.com>
2019-06-17 12:41:12 +0900

    create root cmd

---
+++ internal/jft/jft.go
@@ -0,0 +1,13 @@
+package jft
+
+import "github.com/spf13/cobra"
+
+func Exec() *cobra.Command {
+	cmd := &cobra.Command{
+		// TODO: 時間があれば、説明を充実する
+		Use:   "jft",
+		Short: "calendar cli tool, just for today",
+		Long:  ``,
+	}
+	return cmd
+}
