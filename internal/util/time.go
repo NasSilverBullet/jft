c51c8fc19618120841c255ef2b8a50b888ec1e75
y.t <y.softvalley@gmail.com>
2019-06-18 09:02:19 +0900

    replace model.GetMonthEndAndBeginning

--- internal/util/time.go
+++ internal/util/time.go
@@ -62,3 +62,35 @@ func GetDayEndAndBeginning(dateString string) (*time.Time, *time.Time, error) {
 	end := time.Date(year, time.Month(month), date, 23, 59, 59, 59, time.Local)
 	return &begin, &end, err
 }
+
+func GetMonthEndAndBeginning(dateString string) (*time.Time, *time.Time, error) {
+	const dateformat = `^(19[0-9]{2}|20[0-9]{2})/(0?[1-9]|1[0-2])$`
+	if dateString == "" {
+		n := time.Now()
+		dateString = strconv.Itoa(n.Year()) + "/" + strconv.Itoa(int(n.Month()))
+	}
+	ok, err := regexp.MatchString(dateformat, dateString)
+	if err != nil {
+		return nil, nil, err
+	}
+	if !ok {
+		return nil, nil, errors.New(fmt.Sprintf("[%s] is not matched to month format", dateString))
+	}
+	yearAndMonth := strings.Split(dateString, "/")
+	year, err := strconv.Atoi(yearAndMonth[0])
+	if err != nil {
+		return nil, nil, err
+	}
+	month, err := strconv.Atoi(yearAndMonth[1])
+	if err != nil {
+		return nil, nil, err
+	}
+	nextYear, nextMonth := year, month+1
+	if nextMonth > 12 {
+		nextMonth = 1
+		nextYear++
+	}
+	begin := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.Local)
+	end := time.Date(nextYear, time.Month(nextMonth), 1, 0, 0, 0, 0, time.Local).AddDate(0, 0, -1)
+	return &begin, &end, err
+}
