0811e7862daf115a8054b742043ce65053092e1d
y.t <y.softvalley@gmail.com>
2019-06-18 09:01:11 +0900

    replace model.GetDayEndAndBeginning

--- internal/model/plan.go
+++ internal/model/plan.go
@@ -3,9 +3,7 @@ package model
 import (
 	"errors"
 	"fmt"
-	"regexp"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/NasSilverBullet/jft/internal/util"
@@ -116,7 +114,7 @@ func (p *Plan) Delete(db *gorm.DB) (*Plan, error) {
 }
 
 func FindPlans(db *gorm.DB, dateStr string) ([]Plan, error) {
-	begin, end, err := getDayEndAndBeginning(dateStr)
+	begin, end, err := util.GetDayEndAndBeginning(dateStr)
 	if err != nil {
 		return nil, err
 	}
@@ -129,35 +127,4 @@ func (p Plan) String() string {
 	const layout = "2006-01-02 15:04"
 	const format = "ID : %v\nStart : %v\nEnd : %v\nTitle : %v\nDescription : %v"
 	return fmt.Sprintf(format, p.ID, p.Start.Format(layout), p.End.Format(layout), p.Title, p.Description)
-}
-
-func getDayEndAndBeginning(dateString string) (*time.Time, *time.Time, error) {
-	const dateformat = `^(19[0-9]{2}|20[0-9]{2})/(0?[1-9]|1[0-2])/(0?[1-9]|[1-2][0-9]|3[0-1])$`
-	if dateString == "" {
-		n := time.Now()
-		dateString = strconv.Itoa(n.Year()) + "/" + strconv.Itoa(int(n.Month())) + "/" + strconv.Itoa(n.Day())
-	}
-	ok, err := regexp.MatchString(dateformat, dateString)
-	if err != nil {
-		return nil, nil, err
-	}
-	if !ok {
-		return nil, nil, errors.New(fmt.Sprintf("[%s] is not matched to date format", dateString))
-	}
-	yearAndMonthAndDate := strings.Split(dateString, "/")
-	year, err := strconv.Atoi(yearAndMonthAndDate[0])
-	if err != nil {
-		return nil, nil, err
-	}
-	month, err := strconv.Atoi(yearAndMonthAndDate[1])
-	if err != nil {
-		return nil, nil, err
-	}
-	date, err := strconv.Atoi(yearAndMonthAndDate[2])
-	if err != nil {
-		return nil, nil, err
-	}
-	begin := time.Date(year, time.Month(month), date, 0, 0, 0, 0, time.Local)
-	end := time.Date(year, time.Month(month), date, 23, 59, 59, 59, time.Local)
-	return &begin, &end, err
 }
--- internal/util/time.go
+++ internal/util/time.go
@@ -31,3 +31,34 @@ func ToTime(timeString string) (*time.Time, error) {
 	t := time.Date(n.Year(), n.Month(), n.Day(), hour, min, 0, 0, time.Local)
 	return &t, err
 }
+
+func GetDayEndAndBeginning(dateString string) (*time.Time, *time.Time, error) {
+	const dateformat = `^(19[0-9]{2}|20[0-9]{2})/(0?[1-9]|1[0-2])/(0?[1-9]|[1-2][0-9]|3[0-1])$`
+	if dateString == "" {
+		n := time.Now()
+		dateString = strconv.Itoa(n.Year()) + "/" + strconv.Itoa(int(n.Month())) + "/" + strconv.Itoa(n.Day())
+	}
+	ok, err := regexp.MatchString(dateformat, dateString)
+	if err != nil {
+		return nil, nil, err
+	}
+	if !ok {
+		return nil, nil, errors.New(fmt.Sprintf("[%s] is not matched to date format", dateString))
+	}
+	yearAndMonthAndDate := strings.Split(dateString, "/")
+	year, err := strconv.Atoi(yearAndMonthAndDate[0])
+	if err != nil {
+		return nil, nil, err
+	}
+	month, err := strconv.Atoi(yearAndMonthAndDate[1])
+	if err != nil {
+		return nil, nil, err
+	}
+	date, err := strconv.Atoi(yearAndMonthAndDate[2])
+	if err != nil {
+		return nil, nil, err
+	}
+	begin := time.Date(year, time.Month(month), date, 0, 0, 0, 0, time.Local)
+	end := time.Date(year, time.Month(month), date, 23, 59, 59, 59, time.Local)
+	return &begin, &end, err
+}
